602c0b1d62575d007515b9be56e58562ffe2460b
sevensilvers <jeffpaltera@outlook.com>
2016-03-02 17:41:18 +0800

    first commit

---
+++ mobile/js/main.js
@@ -0,0 +1,181 @@
+/*
+ *
+ * mads - version 2.00.01
+ * Copyright (c) 2015, Ninjoe
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * https://en.wikipedia.org/wiki/MIT_License
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ *
+ */
+var mads = function() {
+  /* Get Tracker */
+  if (typeof custTracker == 'undefined' && typeof rma != 'undefined') {
+    this.custTracker = rma.customize.custTracker;
+  } else if (typeof custTracker != 'undefined') {
+    this.custTracker = custTracker;
+  } else {
+    this.custTracker = [];
+  }
+
+  /* CT */
+  if (typeof ct == 'undefined' && typeof rma != 'undefined') {
+    this.ct = rma.ct;
+  } else if (typeof ct != 'undefined') {
+    this.ct = ct;
+  } else {
+    this.ct = [];
+  }
+
+  /* CTE */
+  if (typeof cte == 'undefined' && typeof rma != 'undefined') {
+    this.cte = rma.cte;
+  } else if (typeof cte != 'undefined') {
+    this.cte = cte;
+  } else {
+    this.cte = [];
+  }
+
+  /* Unique ID on each initialise */
+  this.id = this.uniqId();
+
+  /* Tracked tracker */
+  this.tracked = [];
+  /* each engagement type should be track for only once and also the first tracker only */
+  this.trackedEngagementType = [];
+  /* trackers which should not have engagement type */
+  this.engagementTypeExlude = [];
+  /* first engagement */
+  this.firstEngagementTracked = false;
+
+  /* Body Tag */
+  this.bodyTag = document.getElementsByTagName('body')[0];
+
+  /* Head Tag */
+  this.headTag = document.getElementsByTagName('head')[0];
+
+  /* RMA Widget - Content Area */
+  this.contentTag = document.getElementById('rma-widget');
+
+  /* URL Path */
+  this.path = typeof rma != 'undefined' ? rma.customize.src : '';
+
+  /* Solve {2} issues */
+  for (var i = 0; i < this.custTracker.length; i++) {
+    if (this.custTracker[i].indexOf('{2}') != -1) {
+      this.custTracker[i] = this.custTracker[i].replace('{2}', '{{type}}');
+    }
+  }
+};
+
+/* Generate unique ID */
+mads.prototype.uniqId = function() {
+  return new Date().getTime();
+}
+
+/* Link Opner */
+mads.prototype.linkOpener = function(url) {
+
+  if (typeof url != "undefined" && url != "") {
+
+    if (typeof mraid !== 'undefined') {
+      mraid.open(url);
+    } else {
+      window.open(url);
+    }
+  }
+}
+
+/* tracker */
+mads.prototype.tracker = function(tt, type, name, value) {
+
+  /*
+   * name is used to make sure that particular tracker is tracked for only once
+   * there might have the same type in different location, so it will need the name to differentiate them
+   */
+
+
+  name = name || type;
+
+
+  if (typeof this.custTracker != 'undefined' && this.custTracker != '' && this.tracked.indexOf(name) == -1) {
+    for (var i = 0; i < this.custTracker.length; i++) {
+      var img = document.createElement('img');
+
+      if (typeof value == 'undefined') {
+        value = '';
+      }
+
+      /* Insert Macro */
+      var src = this.custTracker[i].replace('{{type}}', type);
+      src = src.replace('{{value}}', value);
+
+      /* Insert TT's macro */
+      if (this.trackedEngagementType.indexOf(tt) != '-1' || this.engagementTypeExlude.indexOf(tt) != '-1') {
+        src = src.replace('tt={{tt}}', '');
+      } else {
+        src = src.replace('{{tt}}', tt);
+        this.trackedEngagementType.push(tt);
+      }
+
+      /* Append ty for first tracker only */
+      if (!this.firstEngagementTracked && tt == 'E') {
+        src = src + '&ty=E';
+        this.firstEngagementTracked = true;
+      }
+
+      /* */
+      img.src = src + '&' + this.id;
+
+      img.style.display = 'none';
+      this.bodyTag.appendChild(img);
+
+      this.tracked.push(name);
+    }
+  }
+};
+
+/* Load JS File */
+mads.prototype.loadJs = function(js, callback) {
+  var script = document.createElement('script');
+  script.src = js;
+
+  if (typeof callback != 'undefined') {
+    script.onload = callback;
+  }
+
+  this.headTag.appendChild(script);
+}
+
+/* Load CSS File */
+mads.prototype.loadCss = function(href) {
+  var link = document.createElement('link');
+  link.href = href;
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('rel', 'stylesheet');
+
+  this.headTag.appendChild(link);
+}
+
+/*
+ *
+ * Unit Testing for mads
+ *
+ */
+
+
+var Ad = function() {
+  this.sdk = new mads();
+  var self = this;
+
+  self.sdk.loadCss(this.sdk.path + 'css/style.css');
+
+  self.sdk.contentTag.innerHTML = '<iframe style="border:0" width="250" height="210" src="' + self.sdk.path + 'yt.html?custTracker='+escape(JSON.stringify(self.sdk.custTracker))+'" id="video"></iframe>';
+
+  document.getElementById("rma-widget").addEventListener('click', function(e) {
+    e.preventDefault();
+    self.sdk.linkOpener('https://www.youtube.com/watch?v=J5GmGoojzGk&utm_source=mobilewalla&utm_medium=banner&utm_campaign=GGmild');
+    self.sdk.tracker('CTR', 'site');
+  });
+};
+
+var ad = new Ad();
